f8ea6e2882599834f9fbde252f47127871dbb3b2
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2023-10-02 12:35:38 +0300

    Calculator completed

--- mooc-securing-software-23/part1-03.calculator/src/pages/views.py
+++ mooc-securing-software-23/part1-03.calculator/src/pages/views.py
@@ -4,8 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def addPageView(request):
-	return HttpResponse('0')
-	
+    first = int(request.GET.get('first'))
+    second = int(request.GET.get('second'))
+    return HttpResponse(first + second)
+
 
 def multiplyPageView(request):
-	return HttpResponse('0')
+    first = int(request.GET.get('first'))
+    second = int(request.GET.get('second'))
+    return HttpResponse(first * second)
