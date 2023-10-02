bf117791f0d59f2f1fd262b63ea06245b036547c
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2023-10-02 19:30:31 +0300

    Completed Notebook

--- mooc-securing-software-23/part1-06.notebook/src/pages/views.py
+++ mooc-securing-software-23/part1-06.notebook/src/pages/views.py
@@ -2,23 +2,36 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def addPageView(request):
-	items = request.session.get('items', [])
+    items = request.session.get('items', [])
 
+    if request.method == 'POST':
+        item = request.POST.get('content', '').strip()
+        if len(item) > 0:
+            items.append(item)
 
-	return render(request, 'pages/index.html', {'items' : items})
+        if len(items) > 10:
+            items = items[-10:]
+
+        request.session['items'] = items
+
+    return render(request, 'pages/index.html', {'items': items})
 
 
 def erasePageView(request):
-	items = request.session.get('items', [])
+    items = request.session.get('items', [])
 
+    if request.method == 'POST':
+        items = []
+    request.session['items'] = []
 
-	return render(request, 'pages/index.html', {'items' : items})
+    return render(request, 'pages/index.html', {'items': items})
 
 
 def homePageView(request):
-	# use sessions (the data is stored in a database db.sqlite that is then accessed using a cookie)
-	items = request.session.get('items', [])
+    # use sessions (the data is stored in a database db.sqlite that is then accessed using a cookie)
+    items = request.session.get('items', [])
 
-	# shorter way of writing instead of loader
-	return render(request, 'pages/index.html', {'items' : items})
+    # shorter way of writing instead of loader
+    return render(request, 'pages/index.html', {'items': items})
