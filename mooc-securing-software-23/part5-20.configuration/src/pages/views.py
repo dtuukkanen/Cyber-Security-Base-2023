23c276ce5dffd545b65e00ceee969ffb44e509b1
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2023-11-06 22:43:40 +0200

    Bad configuration completed

--- mooc-securing-software-23/part5-20.configuration/src/pages/views.py
+++ mooc-securing-software-23/part5-20.configuration/src/pages/views.py
@@ -8,18 +8,24 @@ from .models import Account
 
 
 @login_required
-@csrf_exempt
+@transaction.atomic
 def transferView(request):
-	
-	if request.method == 'GET':
-		to = User.objects.get(username=request.GET.get('to'))
-		amount = int(request.GET.get('amount'))
-	
-	return redirect('/')
+    if request.method == 'POST':
+        user = request.user
+        to = User.objects.get(username=request.POST.get('to'))
+        amount = int(request.POST.get('amount'))
 
+        if amount >= 0 and user.account.balance >= amount:
+            user.account.balance -= amount
+            to.account.balance += amount
+
+        user.account.save()
+        to.account.save()
+
+    return redirect('/')
 
 
 @login_required
 def homePageView(request):
-	accounts = Account.objects.exclude(user_id=request.user.id)
-	return render(request, 'pages/index.html', {'accounts': accounts})
+    accounts = Account.objects.exclude(user_id=request.user.id)
+    return render(request, 'pages/index.html', {'accounts': accounts})
