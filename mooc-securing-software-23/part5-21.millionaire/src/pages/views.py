e3a6cade0bb3cc58bc28c5ba0f1e053a29de4b8c
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2023-11-07 00:12:56 +0200

    Millionaire completed

--- mooc-securing-software-23/part5-21.millionaire/src/pages/views.py
+++ mooc-securing-software-23/part5-21.millionaire/src/pages/views.py
@@ -2,61 +2,69 @@ from django.shortcuts import render, redirect
 
 from .models import questions
 
+
 def find_topic(tid):
-	for q in questions:
-		if q['id'] == tid:
-			return q
-	return None
+    for q in questions:
+        if q['id'] == tid:
+            return q
+    return None
 
 
 def quizView(request, tid):
-	topic = find_topic(tid)
+    topic = find_topic(tid)
 
-	request.session['level'] = 0
-	return render(request, 'pages/question.html', {'topic' : topic, 'question' : topic['questions'][0]})
-
+    request.session['level'] = 0
+    request.session['topic'] = tid
+    return render(request, 'pages/question.html', {'topic': topic, 'question': topic['questions'][0]})
 
 
 def answerView(request, tid, aid):
-		
-	topic = find_topic(tid)
+    if request.session['topic'] != tid:
+        return redirect('/cheater/')
 
-	level = request.session['level']
+    topic = find_topic(tid)
 
-	if topic['questions'][level]['correct'] == aid:
-		level += 1
-		request.session['level'] = level
+    level = request.session['level']
 
-		if level == len(topic['questions']):
-			return redirect('/finish/')
+    if topic['questions'][level]['correct'] == aid:
+        level += 1
+        request.session['level'] = level
 
-		return render(request, 'pages/question.html', {'topic' : topic, 'question' : topic['questions'][level]})
-	else:
-		return redirect('/incorrect/')
+        if level == len(topic['questions']):
+            request.session['done'] = True
+            request.session['topic'] = -1
+            return redirect('/finish/')
+
+        return render(request, 'pages/question.html', {'topic': topic, 'question': topic['questions'][level]})
+    else:
+        request.session['topic'] = -1
+        return redirect('/incorrect/')
 
 
 def incorrectView(request):
-	return render(request, 'pages/incorrect.html')
+    return render(request, 'pages/incorrect.html')
 
 
 def finishView(request):
-	return render(request, 'pages/finish.html')
+    if 'done' not in request.session or not request.session['done']:
+        return redirect('/cheater/')
+    return render(request, 'pages/finish.html')
 
 
 def cheaterView(request):
-	return render(request, 'pages/cheater.html')
+    return render(request, 'pages/cheater.html')
 
 
 def thanksView(request):
-	# Like we were going to pay anyone
-	return render(request, 'pages/thanks.html')
-
+    request.session['done'] = False
+    # Like we were going to pay anyone
+    return render(request, 'pages/thanks.html')
 
 
 def topicView(request, tid):
-	topic = find_topic(tid)
-	return render(request, 'pages/topic.html', {'topic' : topic})
+    topic = find_topic(tid)
+    return render(request, 'pages/topic.html', {'topic': topic})
 
 
 def topicsView(request):
-	return render(request, 'pages/topics.html', {'questions' : questions})
+    return render(request, 'pages/topics.html', {'questions': questions})
